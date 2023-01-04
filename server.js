b50810a2d84fe9819a0621a2f7f01a9073a5502f
Haley Dawe <hdawe@my.bcit.ca>
2023-01-04 13:57:38 -0800

    fixed the api first try

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+  console.log("GET /")
+  res.send("<h1>hello aws</h1>")
+})
 
 app.use(express.json())
 
