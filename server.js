6c7e67fad059ac093a03ab225e5e1058ac090192
Haley Dawe <hdawe@my.bcit.ca>
2023-01-05 13:37:10 -0800

    fixed issues not sure how

--- server.js
+++ server.js
@@ -7,41 +7,43 @@ app.use(express.json())
 
 app.use(express.static("build"))
 
-const pokemons = [
-  {
-    id: 1,
-    name: "Pikachu",
-    type: "electric ⚡️",
-    level: 99,
-    image: "/pikachu.webp"
+
+
+function Pokemon(id, name, type, level, image) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.image = image;
   }
-]
+  
+  const pokemons = [
+    new Pokemon(1, "Pikachu", "electric ⚡️", 99, "/pikachu.webp")
+  ];
 
-
-app.get("/api/pokemons", (req, res) => {
-  console.log("GET /api/pokemons")
-  res.send({pokemons: pokemons})
-});
-
-
-app.post("/api/pokemons", (req, res) => {
-  const data = req.body
-  console.log("POST /api/pokemons", data)
-
-  const newPokemon = new pokemons({
-    id: parseInt(data.id)+2,
-    name: data.name,
-    type: data.type,
-    level: data.level,
-    image: data.image
+  app.get("/api/pokemons", (req, res) => {
+    console.log("GET /api/pokemons")
+    console.log(res);
+    res.send({pokemons: pokemons})
   });
-
-  pokemons.push(newPokemon)
-  res.send(data)
-
-});
-
-
+  
+  app.post("/api/pokemons", (req, res) => {
+    const data = req.body;
+    console.log("POST /api/pokemons", data);
+  
+    const newPokemon = new Pokemon(
+      data.id+1,
+      data.name,
+      data.type,
+      parseInt(data.level),
+      data.image
+    );
+  
+    pokemons.push(newPokemon);
+    res.send(pokemons);
+  });
+  
+  
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'))
 });
